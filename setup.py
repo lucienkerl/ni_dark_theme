d81453eff8b39c25f95a78e050cdb758f137805b
Lucien Kerl <lucien@kerl.io>
2020-08-16 21:20:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ import re, ast
 # get version from __version__ variable in ni_dark_theme/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
+	
 with open('ni_dark_theme/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
@@ -24,6 +27,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
